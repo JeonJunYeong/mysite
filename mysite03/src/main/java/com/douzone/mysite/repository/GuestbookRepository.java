034c556f94ff1b24af0fdd27e79d865734203481
bit <bit@DESKTOP-ULIDFS7>
2020-03-20 16:47:32 +0900

    valid commit #1

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.douzone.mysite.vo.GuestbookVo;;
 
@@ -28,12 +29,8 @@ public class GuestbookRepository {
 	}
 
 	public List<GuestbookVo> findAll(){
-		
-		
 		List<GuestbookVo> list = sqlSession.selectList("guestbook.findAll");
-		
-		
-			return list;
+		return list;
 	}
 	
 	public boolean delete(Long no, String password) {
