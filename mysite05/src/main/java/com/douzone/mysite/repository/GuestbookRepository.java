d910e52ddf813a22a4bfcd0b05f7b0ea3ab661f8
bit <bit@DESKTOP-ULIDFS7>
2020-03-26 15:22:04 +0900

    mysite05 commit

---
+++ mysite05/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -0,0 +1,52 @@
+package com.douzone.mysite.repository;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
+
+import com.douzone.mysite.vo.GuestbookVo;;
+
+@Repository
+public class GuestbookRepository {
+
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	public boolean insert(GuestbookVo vo) {
+		
+		int count = sqlSession.insert("guestbook.insert",vo);
+		
+		boolean result = count ==1;
+		
+		return result;
+	}
+
+	public List<GuestbookVo> findAll(){
+		List<GuestbookVo> list = sqlSession.selectList("guestbook.findAll");
+		return list;
+	}
+	
+	public boolean delete(Long no, String password) {
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("no",no);
+		map.put("password", password);
+		
+		int count = sqlSession.delete("guestbook.delete",map);
+		
+		boolean result = count ==1;
+
+		return result;
+	}
+	
+	
+	
+	
+}
